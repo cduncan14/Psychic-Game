b4e6a013df2aebbc9ac4b7d150bc779c63a206a2
Christian Duncan <cjduncan14@gmail.com>
2018-10-06 01:33:26 -0700

    Added Document Changes

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -9,6 +9,7 @@ var Loss = 0;
 var compGuess = 1;
 var computerChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var guessesSoFar = [];
+var Games = 0;
 
 function getNewCG() {
     this.compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -31,11 +32,13 @@ document.onkeyup = function (event) {
             Loss = Loss+1;
             Guess = 0;
             getNewCG();
+            Games = Games+1;
         }
         else if ((userGuess == compGuess)) {
             wins = wins+1;
             Guess = 0;
             getNewCG();
+            Games = Games+1;
         }
         else if ((userGuess != compGuess)) {
             Guess = Guess+1;
@@ -44,7 +47,10 @@ document.onkeyup = function (event) {
     console.log("Wins: " + wins + " Guesses: " + Guess + " Losses: " + Loss);
     console.log("Computer Guess: " + compGuess + " Your Guess: " + userGuess);
     console.log("Guesses so Far: " + guessesSoFar)
-    document.getElementById(Guess).innerHTML = guessesSoFar;
+    document.getElementById("Guess").innerText = guessesSoFar;
+    document.getElementById("Wins").innerText = wins;
+    document.getElementById("Loss").innerText = Loss;
+    document.getElementById("Game").innerText = Games;
     }
     if ((isNaN(userGuess) == false)) {
         console.log("Please Input a Letter! :)");
